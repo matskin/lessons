cec53c613968dfd1834b72d58128d7fcac04c0a3
Eugene Matskin <eugene@matskin.net>
2020-11-20 19:06:17 +0200

    Prototyping and inheriting properties from it

--- interestingNuances/script.js
+++ interestingNuances/script.js
@@ -10,3 +10,22 @@ function compareNum(a,b) {
 }
 
 console.log(arr); // [ 1, 4, 15 ]
+
+
+// Урок 2.16 Объектно-ориентированное программирование (ООП)
+// Прототипирование и наследование свойств
+
+let soldier = {
+    health: 400,
+    armor: 200
+};
+
+let john = {
+  health: 100
+};
+
+john.__proto__ = soldier;
+
+console.log(john);
+console.log(john.armor);
+
