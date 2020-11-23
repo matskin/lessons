a5822ba9aae21a007fca4afd3c2d2b26e38f62e5
Eugene Matskin <eugene@matskin.net>
2020-11-23 13:17:05 +0200

    Actions with elements on the page

--- 05/script.js
+++ 05/script.js
@@ -5,3 +5,23 @@ let box = document.getElementById('box'),
     oneHeart = document.querySelector('.heart');
 
     box.style.backgroundColor = 'blue';
+    btn[1].style.borderRadius = '100%';
+
+    circle[0].style.backgroundColor = 'red';
+    circle[1].style.backgroundColor = 'yellow';
+    circle[2].style.backgroundColor = 'green';
+
+    // for (let i = 0; i < heart.length; i++) {
+    //     heart[i].style.backgroundColor = 'blue';
+    // }
+
+    // heart.forEach(function(item) {
+    //     item.style.backgroundColor = 'green';
+    // });
+
+    let div = document.createElement('div'),
+        text = document.createTextNode('Тут был Ваня');
+
+    div.classList.add('black');
+
+    document.body.appendChild(div);
