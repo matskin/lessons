a5333931dbb33eaa93274a7de55fc419e1992720
Eugene Matskin <eugene@matskin.net>
2020-11-24 14:32:53 +0200

    Added new methods

--- 05/script.js
+++ 05/script.js
@@ -2,7 +2,8 @@ let box = document.getElementById('box'),
     btn = document.getElementsByTagName('button'),
     circle = document.getElementsByClassName('circle'),
     heart = document.querySelectorAll('.heart'),
-    oneHeart = document.querySelector('.heart');
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
 
     box.style.backgroundColor = 'blue';
     btn[1].style.borderRadius = '100%';
@@ -24,4 +25,15 @@ let box = document.getElementById('box'),
 
     div.classList.add('black');
 
-    document.body.appendChild(div);
+    // document.body.appendChild(div);
+    // wrapper.appendChild(div);
+
+    // div.innerHTML = '<h1>Hello World!</h1>';
+    div.textContent = 'Hello World!';
+
+    document.body.insertBefore(div, circle[0]);
+    document.body.removeChild(circle[1]);
+    wrapper.removeChild(heart[1]);
+
+    document.body.replaceChild(btn[1], circle[1]);
+
