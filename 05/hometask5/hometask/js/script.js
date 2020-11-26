fad284f6e7f2bdc2cbffd875b741f1dbf95c210c
Eugene Matskin <eugene@matskin.net>
2020-11-26 13:22:51 +0200

    Work on a practical lesson

--- 05/hometask5/hometask/js/script.js
+++ 05/hometask5/hometask/js/script.js
@@ -1,4 +1,15 @@
-let menuItem = document.querySelectorAll('.menu-item');
+let menuItem = document.querySelectorAll('.menu-item'),
+    title = document.querySelector('.title'),
+    menu = document.querySelector('.menu'),
+    text = document.createTextNode('Пятый пункт');
 
 menuItem[1].textContent = 'Второй пункт';
 menuItem[2].textContent = 'Третий пункт';
+
+title.textContent = 'Мы продаем только подлинную технику Apple';
+
+menu = document.createElement('li');
+menu.className = 'menu-item';
+menu.appendChild(text);
+
+console.log(menu-item[5]);
