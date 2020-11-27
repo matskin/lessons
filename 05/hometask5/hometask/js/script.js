a1d9b12a6c2db96fdb2b8dbc3af8b1e552ed4f93
Eugene Matskin <eugene@matskin.net>
2020-11-27 19:20:29 +0200

    Work on a practical lesson

--- 05/hometask5/hometask/js/script.js
+++ 05/hometask5/hometask/js/script.js
@@ -1,15 +1,19 @@
 let menuItem = document.querySelectorAll('.menu-item'),
     title = document.querySelector('.title'),
     menu = document.querySelector('.menu'),
-    text = document.createTextNode('Пятый пункт');
+    text = document.createTextNode('Пятый пункт')
+    col = document.querySelectorAll('.column'),
+    adv = document.querySelector('.adv');
 
 menuItem[1].textContent = 'Второй пункт';
 menuItem[2].textContent = 'Третий пункт';
 
 title.textContent = 'Мы продаем только подлинную технику Apple';
 
-menu = document.createElement('li');
-menu.className = 'menu-item';
-menu.appendChild(text);
+// menu = document.createElement('li');
+// menu.className = 'menu-item';
+// menu.appendChild(text);
 
-console.log(menu-item[5]);
+// console.log(menu-item[5]);  // Есть непонимание, нужно разобраться
+
+// document.col.removeChild(adv); // Нужно разобраться с DOM более досконально
