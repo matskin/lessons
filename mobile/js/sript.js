999e86cda334fac47c33d797de0c7706a0708356
Eugene Matskin <eugene@matskin.net>
2020-12-08 18:43:40 +0200

    MultiTouch and Coordinats

--- mobile/js/sript.js
+++ mobile/js/sript.js
@@ -11,11 +11,16 @@ window.addEventListener('DOMContentLoaded', function() {
     box.addEventListener('touchstart', function(e) {
         e.preventDefault();
         console.log('Red box: touchstart');
+        console.log(e.target);
+        console.log(e.touches[0].target);
+        console.log(e.changedTouches);
+        console.log(e.targetTouches);
     });
 
     box.addEventListener('touchmove', function(e) {
         e.preventDefault();
         console.log('Red box: touchmove');
+        console.log('Red box: ' + e.touches[0].pageX);
     });
 
     box.addEventListener('touchend', function(e) {
