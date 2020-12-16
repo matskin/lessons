57d3810483365b42fbbe3ff8a8ad31e5a99135e7
Eugene Matskin <eugene@matskin.net>
2020-12-16 19:30:48 +0200

    ES6. let and const

--- yoga/js/script2.js
+++ yoga/js/script2.js
@@ -1,10 +1,39 @@
 
-    // Второе задание
+// Второе задание
 
-    let age = document.getElementById('age');
+// let age = document.getElementById('age');
     
-    function showUser(surname, name) {
-            alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// function showUser(surname, name) {
+//     alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+    
+// showUser.apply(age, ["Горький","Максим"]);
+    
+    
+// ES6. let and const
+let name = "Ivan",
+    age = 30,
+    mail = 'ex@gmail.com';
+
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
+
+function makeArray() {
+    var items = [];
+
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
     }
-    
-    showUser.apply(age, ["Горький","Максим"]);
+
+    return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
+
+
