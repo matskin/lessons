54e52d8f363a3352a356347029501317a6e2d3e0
Eugene Matskin <eugene@matskin.net>
2020-12-24 23:14:56 +0200

    The Promise object is used for lazy and asynchronous computation

--- 73/script.js
+++ 73/script.js
@@ -34,3 +34,43 @@ shoot({})
         .then(mark => console.log('Вы попали в цель!'))
         .then(win)
         .catch(loose)
+
+
+// Promise Constructor
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('foo');
+    }, 300);
+  });
+  ​
+  promise1.then((value) => {
+    console.log(value);
+    // expected output: "foo"
+  
+  });
+  
+  ​console.log(promise1);
+  // expected output: [object Promise]
+  ​
+
+// Простой пример
+let myFirstPromise = new Promise((resolve, reject) => {
+    // Мы вызываем resolve(...), когда асинхронная операция завершилась успешно, и reject(...), когда она не удалась.
+    // В этом примере мы используем setTimeout(...), чтобы симулировать асинхронный код.
+    // В реальности вы, скорее всего, будете использовать XHR, HTML5 API или что-то подобное.
+    setTimeout(function(){
+      resolve("Success!"); // Ура! Всё прошло хорошо!
+    }, 250);
+  });
+  
+  myFirstPromise.then((successMessage) => {
+    // successMessage - это что угодно, что мы передали в функцию resolve(...) выше.
+    // Это необязательно строка, но если это всего лишь сообщение об успешном завершении, это наверняка будет она.
+    console.log("Ура! " + successMessage);
+  });
+
+
+// Продвинутый пример
+<button id="btn">Создать Promise!</button>
+<div id="log"></div>
+
